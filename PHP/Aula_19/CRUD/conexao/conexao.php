ca91ee38fcdd17f8cc559d6e7a61e04a3781c67d
CamillyBSantos <camillybarbosasantos@outlook.com>
2025-05-08 10:46:11 -0300

    .
    .

---
+++ PHP/Aula_19/CRUD/conexao/conexao.php
@@ -0,0 +1,18 @@
+<?php
+$_severName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "faculdade";
+
+//Criando conexão
+
+$coon = new mysqli($_severName, $userName, $password, $dbName);
+
+//Validação de Conexão
+if ($coon->connect_error) {
+    echo"Conexão Falhou";}
+
+else{
+    echo"Conexão feita com sucesso!";}    
+
+?>
