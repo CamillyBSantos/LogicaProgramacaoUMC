4b5b291579851b57d6e5dfdd792debcd7a1e41a8
CamillyBSantos <camillybarbosasantos@outlook.com>
2025-05-08 08:34:07 -0300

    .
    .

---
+++ PHP/Aula_18/Formulario/processa.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="stylephp.css">
+</head>
+<body>
+    <main class="container">
+
+        <h1>Dados Enviados</h1>
+        <?php
+           // echo var_dump($_POST);
+            $nome= $_POST ["nome"];
+            $sobrenome= $_POST ["sobrenome"];    
+            $email= $_POST ["email"];
+            $senha= $_POST ["senha"];                      
+ 
+            echo "<p><strong> O seu nome é:  <strong> $nome </p>";
+            echo "<p><strong> O seu sobrenome é:  <strong> $sobrenome </p>";
+            echo "<p><strong> O seu e-mail é:  <strong> $email </p>";
+            echo "<p><strong> A sua senha é:  <strong> $senha </p>";
+    
+ 
+        ?>
+</main>
+
+
+
+</body>
+</html>
