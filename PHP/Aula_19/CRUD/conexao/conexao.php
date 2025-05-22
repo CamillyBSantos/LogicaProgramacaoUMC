25112866b623e5db0212b5f0d08cc214f5ca922b
CamillyBSantos <camillybarbosasantos@outlook.com>
2025-05-22 10:35:37 -0300

    .
    .

--- PHP/Aula_19/CRUD/conexao/conexao.php
+++ PHP/Aula_19/CRUD/conexao/conexao.php
@@ -1,15 +1,16 @@
 <?php
-$_severName = "localhost";
+$serverName = "localhost";
 $userName = "root";
 $password = "";
 $dbName = "faculdade";
 
-//Criando conexão
-$coon = new mysqli($_severName, $userName, $password, $dbName);
+//Criando Conexão
+$conn = new mysqli($serverName, $userName, $password, $dbName);
 
 //Validação de Conexão
-if ($coon->connect_error) {
-    echo"Conexão Falhou";
+if ($conn->connect_error){
+    echo "Conexão Falhou";
 }
+
 
 ?>
--- PHP/Aula_19/CRUD/index.php
+++ PHP/Aula_19/CRUD/index.php
@@ -3,20 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOME</title>
+    <title>Página HOME</title>
     <link rel="stylesheet" href="estilos/style.css">
 </head>
 <body>
+    <h1>Bem-Vindo ao sistema de Cadastro</h1>
+    <p>Escolha umas da opções abaixo</p>
 
-    <h1>Bem-vindo ao sistema de cadastro</h1>
-    <p>Escolha uma das oções a baixo</p>
-    
-    <div>
-        <a href="backend/cadastro.php">Cadastro</a>
-        <a href="backend/verificarCadastro.php">Verificar cadastro</a>
-        <a href="">Atualizar cadastro</a>
-        <a href="">Verificar Nota</a>
-        <a href="">Atualiza nota</a>
+    <div class="menu">
+        <a href="back/cadastro.php">Cadastro</a>
+        <a href="back/verificarCadastro.php">Verificar Cadastro</a>
+        <a href="">Atualizar Cadastro</a>
+        <a href="back/verificarNota.php">Verificar Nota</a>
+        <a href="">Atualizar Nota</a>
     </div>
+
+    
+
 </body>
 </html>
